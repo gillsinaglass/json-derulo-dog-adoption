0dea71030c120c78fc38b77036d072bb2d159e17
github username <github email address>
2019-01-23 15:00:01 -0500

    Dog

--- lib/breed.rb
+++ lib/breed.rb
@@ -1,6 +1,10 @@
 class Breed < ActiveRecord::Base
+  has_many :dogs
+  delegate :adoption, :to => :dogs
 
-
-
+  def self.create_and_save(hash)
+    breed = Breed.create(hash)
+    breed.save
+  end
 
 end
