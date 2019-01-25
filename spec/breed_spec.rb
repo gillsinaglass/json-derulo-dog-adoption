85417abf223258ca0d5e8b77a25309ce71ab9a89
github username <github email address>
2019-01-25 12:56:50 -0500

    breed specs are existent unlike the puggle that died

--- spec/breed_spec.rb
+++ spec/breed_spec.rb
@@ -4,8 +4,19 @@ require_relative '../config/environment.rb'
 describe Breed do
 
  let (:porgi) {Breed.create(name: "Porgi", breed_group: "mutant")}
+ let (:german_shep) {Breed.create(name: "German Shepherd", breed_group: "Herding")}
+ let (:puggle) {Breed.create(name: "Puggle", temperament: "great, but dead")}
 
- it 'contains name' do
-   expect(porgi.name == "Porgi")
- end
+   it 'contains name' do
+     expect(porgi.name == "Porgi")
+   end
+
+   it 'contains breed group' do
+     expect(german_shep.breed_group == "Herding")
+   end
+
+   it 'contains breed temperament' do
+     expect(puggle.temperament == "great, but dead")
+   end
+
 end
